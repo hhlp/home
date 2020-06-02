fd13a2ccf6b393dcf6f87f2e2a973829105a9477
Héctor Louzao <louzaoh@gmail.com>
2020-06-02 12:03:44 +0200

    Updates

---
+++ precache-manifest.531ea8b8506c1f18fed7eac4bdf0840f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf752c6c23153b872247b1ab2dadd120",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "95ea5c2ae10eb35bb877",
+    "url": "/home/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ae13b7fb1990ea331c35",
+    "url": "/home/static/css/main.5f05969b.chunk.css"
+  },
+  {
+    "revision": "95ea5c2ae10eb35bb877",
+    "url": "/home/static/js/2.448dd0be.chunk.js"
+  },
+  {
+    "revision": "b71cb3cccc73e099d412c24a04bf9105",
+    "url": "/home/static/js/2.448dd0be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae13b7fb1990ea331c35",
+    "url": "/home/static/js/main.4aa26056.chunk.js"
+  },
+  {
+    "revision": "5c07ecca0456e28e04f0",
+    "url": "/home/static/js/runtime-main.e084aacb.js"
+  },
+  {
+    "revision": "0c246fa08e8969160ef2f65ef1cc4776",
+    "url": "/home/static/media/resume.0c246fa0.pdf"
+  }
+]);
